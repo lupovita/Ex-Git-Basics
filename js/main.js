ca33ad44f26599560b6d0661a40fff8cf334dbd1
Avi Lupovitch <lupovita@post.bgu.ac.il>
2026-05-22 09:21:46 +0300

    Add util.js with 2 functions in it and use the functions in the implementation of onBallClick in main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+'use strict'
+
 const ball = {diameter: 100};
+
 function onBallClick(elBall) {
-    ball.diameter += 50;
+    ball.diameter += getRandomInt(20, 60);
     if (ball.diameter > 400) ball.diameter = 100;
     elBall.style.width = `${ball.diameter}px`;
     elBall.innerText = ball.diameter;
+    elBall.style.backgroundColor = getRandomColor();
 }
---
+++ js/util.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        const randIdx = getRandomInt(0, letters.length);
+        color += letters.charAt(randIdx);
+    }
+    return color;
+}
