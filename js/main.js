f380c302ba964b54996342d9bfca25d57cb8bfc2
Avi Lupovitch <lupovita@post.bgu.ac.il>
2026-05-22 09:55:44 +0300

    Add helper func  and a parameter to onBallClick And modify the model to support multiple balls.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 'use strict'
 
-const ball = {diameter: 100};
+const balls = [
+    {diameter: 100, className: 'ball1'},
+    {diameter: 100, className: 'ball2'}
+];
 
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
+    const ball = getBallModelByElement(elBall);
     ball.diameter += getRandomInt(20, 60);
-    if (ball.diameter > 400) ball.diameter = 100;
+    if (ball.diameter > maxDiameter) ball.diameter = 100;
     elBall.style.width = `${ball.diameter}px`;
     elBall.innerText = ball.diameter;
     elBall.style.backgroundColor = getRandomColor();
+}
+
+function getBallModelByElement(elBall) {
+    for (const ball of balls) {
+        if (elBall.classList.contains(ball.className)) return ball;
+    }
+    return null;
 }
